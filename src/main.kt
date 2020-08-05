2c3f2fea9189bc3eeeba92e96508c5470f1600fc
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-08-05 19:45:29 -0300

    Criacao de classe e objetos.

--- src/main.kt
+++ src/main.kt
@@ -1,12 +1,42 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println()
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+
+    println()
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+private fun testaLacos() {
     var i = 0
-    while (i < 5){
+    while (i < 5) {
 
         i++
 
-        if(i == 3){
+        if (i == 3) {
             continue
             //break
         }
