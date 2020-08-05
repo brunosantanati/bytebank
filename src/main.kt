bbf9198b604579c371cc25cf657f9cea6683c8a4
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-08-05 12:53:56 -0300

    Usar val e var e definir explicitamente tipo das variaveis.

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,12 @@
 fun main(){
     println("Bem vindo ao Bytebank")
 
-    val titular = "Bruno"
+    val titular: String = "Bruno"
+    val numeroConta: Int = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
     println("titular: $titular")
+    println("número da conta: $numeroConta")
+    println("saldo da conta: $saldo")
 }
